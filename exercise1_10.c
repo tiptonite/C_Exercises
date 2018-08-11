71426b97443c9ffe25e8e67df53fe7760893d443
Nathan Tipton <nathan.tipton@aggiemail.usu.edu>
2018-08-11 13:05:01 -0600

    copy input to output, replacing tabs, backspaces, and backslashes with \t, \b, \

---
+++ exercise1_10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+
+  char c;
+  while((c=getchar()) != EOF)
+    {
+      if(c=='\t')
+	printf("\\t");
+      else if(c=='\b')
+	printf("\\b");
+      else if(c=='\\')
+	printf("\\\\");
+      else putchar(c);
+
+
+    }
+
+
+
+
+
+
+
+}
